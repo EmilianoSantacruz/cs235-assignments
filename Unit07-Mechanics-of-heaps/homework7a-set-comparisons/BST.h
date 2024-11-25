53e3c9721f460c31b3b656e194ba90a0bfe69b44
Emiliano Santacruz <mxesco@byu.edu>
2024-11-24 22:07:16 -0700

    Update BST.h

--- Unit07-Mechanics-of-heaps/homework7a-set-comparisons/BST.h
+++ Unit07-Mechanics-of-heaps/homework7a-set-comparisons/BST.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iostream>
+
 template<class T>
 class BST {
 public:
@@ -11,41 +13,126 @@ public:
         Node(T v) : left(nullptr), right(nullptr), value(v) {}
     };
 
-    BST() {
-        // implement the contructor here
-    }
+    BST() : root(nullptr), treeSize(0) {}
 
     ~BST() {
-        // implement the destructor here
+        clear();
     }
 
     const Node* getRootNode() const {
-        // implement getRootNode here
-        // return a pointer to the tree's root node
+        return root;
     }
 
     bool insert(T item) {
-        // implement insert here
-        // return true if item was inserted, false if item was already in the tree
+        if (!root) {
+            root = new Node(item);
+            treeSize++;
+            return true;
+        }
+        return insertHelper(root, item);
     }
 
     bool remove(T item) {
-        // implement remove here
-        // return true if item was removed, false if item wasn't in the tree
+        bool removed = false;
+        root = removeHelper(root, item, removed);
+        if (removed) treeSize--;
+        return removed;
     }
 
     bool contains(T item) const {
-        // implement contains here
-        // return true if item is in the tree, false otherwise
+        return containsHelper(root, item);
     }
 
     void clear() {
-        // implement clear here
-        // remove all nodes from the tree
+        clearHelper(root);
+        root = nullptr;
+        treeSize = 0;
     }
 
     int size() const {
-        // implement size here
-        // return the number of nodes in the tree
+        return treeSize;
+    }
+
+private:
+    Node* root;
+    int treeSize;
+
+    bool insertHelper(Node* node, T item) {
+        if (item == node->value) {
+            return false; // Duplicate item
+        }
+        if (item < node->value) {
+            if (node->left) {
+                return insertHelper(node->left, item);
+            } else {
+                node->left = new Node(item);
+                treeSize++;
+                return true;
+            }
+        } else {
+            if (node->right) {
+                return insertHelper(node->right, item);
+            } else {
+                node->right = new Node(item);
+                treeSize++;
+                return true;
+            }
+        }
+    }
+
+    Node* removeHelper(Node* node, T item, bool& removed) {
+        if (!node) {
+            removed = false;
+            return nullptr;
+        }
+
+        if (item < node->value) {
+            node->left = removeHelper(node->left, item, removed);
+        } else if (item > node->value) {
+            node->right = removeHelper(node->right, item, removed);
+        } else {
+            removed = true;
+            if (!node->left && !node->right) {
+                delete node;
+                return nullptr;
+            }
+            if (!node->left) {
+                Node* temp = node->right;
+                delete node;
+                return temp;
+            }
+            if (!node->right) {
+                Node* temp = node->left;
+                delete node;
+                return temp;
+            }
+            Node* iop = findInOrderPredecessor(node->left);
+            node->value = iop->value;
+            node->left = removeHelper(node->left, iop->value, removed);
+        }
+        return node;
+    }
+
+    bool containsHelper(Node* node, T item) const {
+        if (!node) return false;
+        if (item == node->value) return true;
+        if (item < node->value) return containsHelper(node->left, item);
+        return containsHelper(node->right, item);
+    }
+
+    void clearHelper(Node* node) {
+        if (!node) return;
+        clearHelper(node->left);
+        clearHelper(node->right);
+        delete node;
+    }
+
+    Node* findInOrderPredecessor(Node* node) {
+        while (node->right) {
+            node = node->right;
+        }
+        return node;
     }
 };
+
+
