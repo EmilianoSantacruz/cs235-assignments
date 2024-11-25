762e138b9fc15b0025b0c03452820433ab02d37c
Emiliano Santacruz <mxesco@byu.edu>
2024-11-24 22:07:58 -0700

    Update VectorSet.h

--- Unit07-Mechanics-of-heaps/homework7a-set-comparisons/VectorSet.h
+++ Unit07-Mechanics-of-heaps/homework7a-set-comparisons/VectorSet.h
@@ -1,29 +1,44 @@
 #pragma once
+#include <vector>
+#include <algorithm>
 
 template<class T>
 class VectorSet {
+
+    std::vector<T> _vec;
+
 public:
+
     bool contains(T item) const {
-        return false;
+        return std::find(_vec.begin(), _vec.end(), item) != _vec.end();
     }
 
     bool insert(T item) {
-        return false;
+        if (!contains(item)) {
+            _vec.push_back(item);
+            return true;
+        }
+        return false;  // Item already exists
     }
 
     bool remove(T item) {
+        auto it = std::find(_vec.begin(), _vec.end(), item);
+        if (it != _vec.end()) {
+            _vec.erase(it);
+            return true;
+        }
         return false;
     }
 
     int size() const {
-        return 0;
+        return _vec.size();
     }
 
     bool empty() const {
-        return true;
+        return _vec.empty();
     }
 
     void clear() {
-
+        _vec.clear();
     }
 };
